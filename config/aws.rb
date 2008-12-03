967e81fb5072ece4b2254ed6950a2582c43c8c3b
Denis Barushev <barushev@gmail.com>
2008-12-03 10:11:50 +0200

    Switched panda to work with local SimpleDB replacement.

--- config/aws.rb
+++ config/aws.rb
@@ -1,4 +1,5 @@
-Amazon::SDB::Base::BASE_PATH = Panda::Config[:sdb_base_url]
+Amazon::SDB::Base::BASE_PATH = 'http://127.0.0.1:8088'
+#Amazon::SDB::Base::BASE_PATH = Panda::Config[:sdb_base_url]
 
 SimpleDB::Base.establish_connection!(
   :access_key_id     => Panda::Config[:access_key_id],
